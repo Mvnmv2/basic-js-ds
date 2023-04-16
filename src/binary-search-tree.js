a5ff1a69d9798cbe5d4fbbafe6c83a7309685879
Andrey <maven23@rambler.ru>
2023-04-16 18:54:00 +0300

    solve: Bs pt1

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,64 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const {Node} = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+    constructor() {
+        this.sroot = null;
+    }
 
-  root() {
+
+    root() {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        if (this.sroot == null) {
+            return null;
+        }
+        return this.sroot;
+    }
+
+
+add(/* data */)
+{
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-  }
+}
 
-  add(/* data */) {
+has(/* data */)
+{
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-  }
+}
 
-  has(/* data */) {
+find(/* data */)
+{
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-  }
+}
 
-  find(/* data */) {
+remove(/* data */)
+{
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-  }
+}
 
-  remove(/* data */) {
+min()
+{
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-  }
+}
 
-  min() {
+max()
+{
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+}
 }
 
 module.exports = {
-  BinarySearchTree
+    BinarySearchTree
 };
