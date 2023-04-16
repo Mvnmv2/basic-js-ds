9235a3a21ee6c386ba0a3d16c234fd6e1363e312
Andrey <maven23@rambler.ru>
2023-04-16 19:57:11 +0300

    solve: Bs pt6, 7

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -123,14 +123,29 @@ class BinarySearchTree {
     }
 
     min() {
-        throw new NotImplementedError('Not implemented');
-        // remove line with error and write your code here
+        if (!this.sroot) {
+            return null;
+        }
+        let node = this.sroot;
+        while (node.left){
+            node = node.left
+        }
+
+        return node.data;
     }
 
     max() {
-        throw new NotImplementedError('Not implemented');
-        // remove line with error and write your code here
+        if (!this.sroot) {
+            return null;
+        }
+        let node = this.sroot;
+        while (node.right){
+            node = node.right
+        }
+
+        return node.data;
     }
+
 }
 
 module.exports = {
